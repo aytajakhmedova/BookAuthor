bacb96be96734060d0310befd8f544fb4574d50a
aytajakhmedova <aytajeah@code.edu.az>
2026-05-19 23:04:48 +0400

    first commit

---
+++ BookAuthor/Services/AuthorService.cs
@@ -0,0 +1,55 @@
+﻿using BookAuthor.Models;
+using BookAuthor.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookAuthor.Services
+{
+    public class AuthorService : IauthorService
+    {
+        private Author[] _authors = [];
+
+        public Author[] GetAll()
+        {
+            return _authors;
+        }
+
+        public Author GetById(int id)
+        {
+            return _authors.FirstOrDefault(a => a.Id == id);
+        }
+
+        public Author[] SearchByFullName(string name)
+        {
+            return _authors
+                .Where(a => a.FullName.ToLower().Contains(name.ToLower()))
+                .ToArray();
+        }
+
+        public Author[] FilterByAge(int age)
+        {
+            return _authors.Where(a => a.Age == age).ToArray();
+        }
+
+        public Author[] FilterByAddress(string address)
+        {
+            return _authors
+                .Where(a => a.Address.ToLower().Contains(address.ToLower()))
+                .ToArray();
+        }
+
+        public void Create(Author author)
+        {
+            Array.Resize(ref _authors, _authors.Length + 1);
+            _authors[1] = author;
+        }
+
+        public void Delete(int id)
+        {
+            _authors = _authors.Where(a => a.Id != id).ToArray();
+        }
+    }
+}
---
+++ BookAuthor/Services/Interfaces/IauthorService.cs
@@ -0,0 +1,20 @@
+﻿using BookAuthor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookAuthor.Services.Interfaces
+{
+    public interface  IauthorService
+    {
+        Author[] GetAll();
+        Author GetById(int id);
+        Author[] SearchByFullName(string name);
+        Author[] FilterByAge(int age);
+        Author[] FilterByAddress(string address);
+        void Create(Author author);
+        void Delete(int id);
+    }
+}
